ee442d92a8b65711382ccb6764838b30f9eb8edd
Laurent Voisard <laurentvoisard1@gmail.com>
2022-09-30 13:29:58 -0400

    Part 1 of pathfinding: Adding nodes and associating them with walls

--- Comp376/Assets/Scripts/ArenaSetup.cs
+++ Comp376/Assets/Scripts/ArenaSetup.cs
@@ -1,32 +1,144 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ArenaSetup : MonoBehaviour
 {
-    [SerializeField] private GameObject _wallSegment;
+    [SerializeField] private WallSegment _wallSegment;
     [SerializeField] private Transform _floor;
     [SerializeField] private Transform _zAxis;
-    private Transform _xAxis;
+    [SerializeField] private Transform _xAxis;
     private int _buildingLayer;
+
+    private WallSegment[,] wallSegmentsX;
+    private WallSegment[,] wallSegmentsZ;
+    private PathNode[,] pathNodeGrid;
+
+    private int maxX;
+    private int maxZ;
 
     void Start()
     {
-        int maxX = ((int)_floor.localScale.x * 10 / 2 - 5);
-        int maxZ = ((int)_floor.localScale.z * 10 / 2);
+        maxX = (int)_floor.localScale.x;
+        maxZ = (int)_floor.localScale.z;
+
+        InstantiateWallSegments();
+        CreatePathNodes();
+        DebugNodeWalls();
+    }
+
+    private void InstantiateWallSegments()
+    {
+        int xOffset = maxX * 10 / 2 - 5;
+        int zOffset = maxZ * 10 / 2;
+
+        wallSegmentsX = new WallSegment[maxX, maxZ];
+        wallSegmentsZ = new WallSegment[maxX, maxZ];
+
         _buildingLayer = LayerMask.NameToLayer("Building");
 
-        for (int x = -maxX; x < maxX + 5; x += 10)
+        for (int x = 1; x < maxX; x++)
         {
-            for (int z = -maxZ + 10; z < maxZ; z += 10)
+            for (int z = 0; z < maxZ; z++)
             {
-                GameObject o = Instantiate(_wallSegment, new Vector3(x, 0, z), Quaternion.identity, _zAxis);
-                o.name = "Wall (" + x + "," + z + ")";
-                o.layer = _buildingLayer;
+                Vector3 wallPosition = new Vector3(x * 10 - xOffset, transform.position.y, z * 10 - zOffset) - Vector3.back * 5f + Vector3.left * 5;
+                WallSegment o = Instantiate(_wallSegment, wallPosition, Quaternion.Euler(0, 90, 0), _xAxis);
+                o.gameObject.name = "Wall (" + x + "," + z + ")";
+                o.gameObject.layer = _buildingLayer;
+                wallSegmentsX[x, z] = o;
             }
         }
 
-        _xAxis = Instantiate(_zAxis, Vector3.zero, Quaternion.Euler(0, 90, 0), transform);
-        _xAxis.name = "X-Axis";
+        for (int x = 0; x < maxX; x++)
+        {
+            for (int z = 1; z < maxZ; z++)
+            {
+                Vector3 wallPosition = new Vector3(x * 10 - xOffset, transform.position.y, z * 10 - zOffset);
+                WallSegment o = Instantiate(_wallSegment, wallPosition, Quaternion.identity, _zAxis);
+                o.gameObject.name = "Wall (" + x + "," + z + ")";
+                o.gameObject.layer = _buildingLayer;
+                wallSegmentsZ[x, z] = o;
+            }
+        }
+
+        //_xAxis = Instantiate(_zAxis, Vector3.zero, Quaternion.Euler(0, 90, 0), transform);
+        //_xAxis.name = "X-Axis";
+    }
+
+    private void CreatePathNodes()
+    {
+        int xOffset = maxX * 10 / 2 - 5;
+        int zOffset = maxZ * 10 / 2 - 5;
+
+        pathNodeGrid = new PathNode[maxX, maxZ];
+
+        for (int x = 0; x < maxX; x++)
+        {
+            for (int z = 0; z < maxZ; z++)
+            {
+                Vector3 nodePosition = new Vector3(x * 10 - xOffset, transform.position.y, z * 10 - zOffset);
+                PathNode node = pathNodeGrid[x, z] = new PathNode(x, z, nodePosition);
+
+                node.SetWalls(wallSegmentsX, wallSegmentsZ, maxX, maxZ);
+
+                //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //cube.transform.position = node.position;
+                //cube.name = "Node " + node.ToString();
+                //cube.transform.parent = transform;
+            }
+        }
+
+        foreach (PathNode node in pathNodeGrid)
+        {
+            node.SetNeighbours(pathNodeGrid, maxX - 1, maxZ - 1);
+        }
+    }
+
+
+    private void DebugNodeWalls()
+    {
+        foreach (PathNode node in pathNodeGrid)
+        {
+            Color wallColor = new Color(UnityEngine.Random.Range(0F, 1F), UnityEngine.Random.Range(0, 1F), UnityEngine.Random.Range(0, 1F)); ;
+            GameObject cubeNode = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cubeNode.transform.position = node.position;
+            cubeNode.GetComponent<MeshRenderer>().material.color = wallColor;
+            cubeNode.name = node.ToString();
+
+
+            if (node.topWall != null)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = node.topWall.transform.position - Vector3.forward;
+                cube.name = "top wall";
+                cube.GetComponent<MeshRenderer>().material.color = wallColor;
+            }
+
+            if (node.bottomWall != null)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = node.bottomWall.transform.position - Vector3.back;
+                cube.name = "bottom wall";
+                cube.GetComponent<MeshRenderer>().material.color = wallColor;
+            }
+
+            if (node.rightWall != null)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = node.rightWall.transform.position - Vector3.right;
+                cube.name = "right wall";
+                cube.GetComponent<MeshRenderer>().material.color = wallColor;
+            }
+
+
+            if (node.leftWall != null)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = node.leftWall.transform.position - Vector3.left;
+                cube.name = "left wall";
+                cube.GetComponent<MeshRenderer>().material.color = wallColor;
+            }
+        }
     }
 }
--- Comp376/Assets/Scripts/Pathfinding/PathNode.cs
+++ Comp376/Assets/Scripts/Pathfinding/PathNode.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PathNode
 {
     // coordinates on the grid
-    private readonly int x, y;
+    private readonly int x, z;
+    public readonly Vector3 position;
 
     public int hScore;
     public int fScore;
@@ -14,46 +15,66 @@ public class PathNode
     public WallSegment topWall, bottomWall, rightWall, leftWall;
     public PathNode topNode, bottomNode, rightNode, leftNode;
 
-    public PathNode(int x, int y)
+    public PathNode(int x, int z, Vector3 position)
     {
         this.x = x;
-        this.y = y;
+        this.z = z;
+        this.position = position;
+    }
+
+    public override string ToString()
+    {
+        return "X: " + x + " z: " + z;
     }
 
     public void SetNeighbours(PathNode[,] grid, int width, int height)
     {
-        if (y < height)
+        if (z < height)
         {
-            topNode = grid[x, y + 1];
+            topNode = grid[x, z + 1];
         }
         
-        if (y > 0)
+        if (z > 0)
         {
-            bottomNode = grid[x, y - 1];
+            bottomNode = grid[x, z - 1];
         }
 
-        if (x < width)
+        if (x < width - 1)
         {
-            rightNode = grid[x + 1, y];
+            rightNode = grid[x + 1, z];
         }
 
         if (x > 0)
         {
-            leftNode = grid[x - 1, y];
+            leftNode = grid[x - 1, z];
         }
+    }
+
+    public void SetWalls(WallSegment[,] wallsX, WallSegment[,] wallsZ, int width, int height)
+    {
+        Debug.Log(ToString());
+
+        if(z > 0)
+            bottomWall = wallsZ[x, z];
+        if(z < height - 1)
+            topWall = wallsZ[x, z + 1];
+        if (x > 0)
+            leftWall = wallsX[x, z];
+        if (x < width - 1)
+            rightWall = wallsX[x + 1, z];
     }
 
     public List<PathNode> GetAvailableNeighbours()
     {
         List<PathNode> neighbours = new List<PathNode>();
-
-        if (topWall.GetWallState() == WallAutomata.WallState.Empty)
+        // temp null check
+        if (topNode != null && (topWall == null || topWall.GetWallState() == WallAutomata.WallState.Empty))
             neighbours.Add(topNode);
-        if (bottomWall.GetWallState() == WallAutomata.WallState.Empty)
+        if (bottomNode != null && (bottomWall == null || bottomWall.GetWallState() == WallAutomata.WallState.Empty))
             neighbours.Add(bottomNode);
-        if (rightWall.GetWallState() == WallAutomata.WallState.Empty)
+        if (rightNode != null && (rightWall == null || rightWall.GetWallState() == WallAutomata.WallState.Empty))
             neighbours.Add(rightNode);
-        if (leftWall.GetWallState() == WallAutomata.WallState.Empty)
+        if (leftNode != null && (leftWall == null || leftWall.GetWallState() == WallAutomata.WallState.Empty))
             neighbours.Add(leftNode);
 
         return neighbours;
