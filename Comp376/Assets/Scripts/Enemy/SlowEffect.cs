bdde06cf557383d51577df5306abb44c910c761c
other604 <94246994+other604@users.noreply.github.com>
2022-12-07 18:57:44 -0500

    added slow effect

---
+++ Comp376/Assets/Scripts/Enemy/SlowEffect.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowEffect : MonoBehaviour
+{
+    //private Animator mAnimator;
+    private SpriteRenderer mSpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //mAnimator = GetComponent<Animator>();
+        mSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnableSlow()
+    {
+        //gameObject.SetActive(true);
+        //mAnimator.enabled = true;
+        mSpriteRenderer.enabled = true;
+    }
+
+    public void DisableSlow()
+    {
+        //gameObject.SetActive(false);
+        //mAnimator.enabled = false;
+        mSpriteRenderer.enabled = false;
+    }
+}
