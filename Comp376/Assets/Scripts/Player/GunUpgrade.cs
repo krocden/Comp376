61e33d411aee13193aa2e9689d0678dfcf57fbab
krocden <kevin.hong715@gmail.com>
2022-12-07 20:23:06 -0500

    Upgrade Balance Change

--- Comp376/Assets/Scripts/Player/GunUpgrade.cs
+++ Comp376/Assets/Scripts/Player/GunUpgrade.cs
@@ -43,33 +43,33 @@ public class GunUpgrade : MonoBehaviour
     {
         switch (id)
         {
-            case 2: rifle.damage *= 1.1f; break;    // Rifle id 2-13
-            case 3: rifle.damage *= 1.15f; break;
-            case 4: rifle.damage *= 1.2f; break;
-            case 5: rifle.damage *= 1.25f; break;
+            case 2: rifle.damage *= 1.15f; break;    // Rifle id 2-13
+            case 3: rifle.damage *= 1.20f; break;
+            case 4: rifle.damage *= 1.25f; break;
+            case 5: rifle.damage *= 1.30f; break;
             case 6: rifle.headshotMultiplier = 1.3f; break;
             case 7: rifle.headshotMultiplier = 1.4f; break;
             case 8: rifle.headshotMultiplier = 1.5f; break;
             case 9: rifle.headshotMultiplier = 1.6f; break;
-            case 10: case 11: case 12: case 13: rifle.magazineSize += 4; rifle.maxAmmo += 20; break;
-            case 15: shotgun.damage *= 1.1f; break;    // Shotgun id 15-26
-            case 16: shotgun.damage *= 1.15f; break;
-            case 17: shotgun.damage *= 1.2f; break;
-            case 18: shotgun.damage *= 1.25f; break;
-            case 19: shotgun.fireRate = 0.55f; break;
-            case 20: shotgun.fireRate = 0.50f; break;
-            case 21: shotgun.fireRate = 0.45f; break;
+            case 10: case 11: case 12: case 13: rifle.magazineSize += 4; rifle.maxAmmo += 40; break;
+            case 15: shotgun.damage *= 1.15f; break;    // Shotgun id 15-26
+            case 16: shotgun.damage *= 1.20f; break;
+            case 17: shotgun.damage *= 1.25f; break;
+            case 18: shotgun.damage *= 1.30f; break;
+            case 19: shotgun.fireRate = 0.64f; break;
+            case 20: shotgun.fireRate = 0.56f; break;
+            case 21: shotgun.fireRate = 0.48f; break;
             case 22: shotgun.fireRate = 0.40f; break;
-            case 23: case 24: case 25: case 26: shotgun.magazineSize += 2; shotgun.maxAmmo += 10; break;
+            case 23: case 24: case 25: case 26: shotgun.magazineSize += 1; shotgun.maxAmmo += 12; break;
             case 28: launcher.explosionRadius = 4f; break;    // launcher id 28-39
-            case 29: launcher.explosionRadius = 5f; break;
-            case 30: launcher.explosionRadius = 6f; break;
-            case 31: launcher.explosionRadius = 7f; break;
-            case 32: launcher.damage *= 1.1f; break;
-            case 33: launcher.damage *= 1.15f; break;
-            case 34: launcher.damage *= 1.2f; break;
-            case 35: launcher.damage *= 1.25f; break;
-            case 36: case 37: case 38: case 39: launcher.magazineSize += 2; launcher.maxAmmo += 6; break;
+            case 29: launcher.explosionRadius = 5.5f; break;
+            case 30: launcher.explosionRadius = 7f; break;
+            case 31: launcher.explosionRadius = 8.5f; break;
+            case 32: launcher.damage *= 1.15f; break;
+            case 33: launcher.damage *= 1.20f; break;
+            case 34: launcher.damage *= 1.25f; break;
+            case 35: launcher.damage *= 1.3f; break;
+            case 36: case 37: case 38: case 39: launcher.magazineSize += 1; launcher.maxAmmo += 8; break;
             default:
                 break;
         }
